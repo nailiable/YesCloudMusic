568c22d396c776a527d521291b62b29d77120851
Zero <1203970284@qq.com>
2024-05-20 00:07:49 +0800

    feat: 404 page, item.vue

---
+++ src/pages/[...all].vue
@@ -0,0 +1,9 @@
+<script setup lang="tsx"></script>
+
+<template>
+  <div mt40 flex justify-center>
+    <h1 title-1>
+      404 Not Found
+    </h1>
+  </div>
+</template>
--- src/typed-router.d.ts
+++ src/typed-router.d.ts
@@ -19,11 +19,13 @@ declare module 'vue-router/auto-routes' {
    */
   export interface RouteNamedMap {
     '/': RouteRecordInfo<'/', '/', Record<never, never>, Record<never, never>>,
+    '/[...all]': RouteRecordInfo<'/[...all]', '/:all(.*)', { all: ParamValue<true> }, { all: ParamValue<false> }>,
     '/category': RouteRecordInfo<'/category', '/category', Record<never, never>, Record<never, never>>,
     '/login/': RouteRecordInfo<'/login/', '/login', Record<never, never>, Record<never, never>>,
     '/my': RouteRecordInfo<'/my', '/my', Record<never, never>, Record<never, never>>,
     '/server/': RouteRecordInfo<'/server/', '/server', Record<never, never>, Record<never, never>>,
     '/server/help': RouteRecordInfo<'/server/help', '/server/help', Record<never, never>, Record<never, never>>,
     '/setting': RouteRecordInfo<'/setting', '/setting', Record<never, never>, Record<never, never>>,
+    '/song-list/item': RouteRecordInfo<'/song-list/item', '/song-list/item', Record<never, never>, Record<never, never>>,
   }
 }
