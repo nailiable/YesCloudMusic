b775d777ef0e33fa01c53922b7f328db1fbe8304
Naily <zero@naily.cc>
2024-07-09 00:53:14 +0800

    fix(playlist): exports

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
 // https://github.com/vueuse/head
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
+import {} from 'vue-music-playlist'
+
 useHead({
   title: 'Vitesse',
   meta: [
